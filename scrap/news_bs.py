710ad742d8a75d72a1b8ecb2db343ec6c0db02b1
numero728 <numero728@naver.com>
2020-12-16 19:55:33 +0900

    scrap

---
+++ scrap/news_bs.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import html5lib
+import requests
+
+conn=0
+try:
+    target_site = 'https://finance.naver.com/news/'
+    res = requests.get(target_site)
+    soup = BeautifulSoup(res.text, 'html5lib')
+    main_news = soup.select('div.main_news > ul > li')
+    results = list()
+    for news in main_news:
+        dic = {
+            'title' : news.a.string
+            ,'href': news.a.get('href')
+        }
+        results.append(dic)
+    import pandas as pd
+    import pymysql
+    import sqlalchemy
+    from sqlalchemy import create_engine
+    import pandas.io.sql as pSql
+    protocal = 'mysql+pymysql'
+    user     = 'admin'
+    password = 'pnudb960726!'
+    domain   = 'personaldb.cepsu2i8bkn5.ap-northeast-2.rds.amazonaws.com'
+    port = 3306
+    database = 'yaneodoo'
+    db_url = f'{protocal}://{user}:{password}@{domain}:{port}/{database}'
+    df = pd.DataFrame(results)
+    engine = create_engine(db_url, encoding = 'utf8')
+    conn = engine.connect()
+    df.to_sql(name = 'main_news2', con = conn, if_exists='replace', index=False)
+except Exception as e:
+    print(e)
+finally:
+    conn.close()
---
+++ scrap/tube.py
@@ -0,0 +1,73 @@
+from selenium import webdriver as wd
+import platform
+import time
+import urllib
+
+driver=0
+conn=0
+try:
+    driver = wd.Chrome('./chromedriver.exe')
+    keyword = urllib.parse.quote('주식') 
+    target_site = f'https://www.youtube.com/results?search_query={keyword}&sp=CAMSBAgCEAE%253D'
+    driver.get( target_site )
+
+    for n in range(5):
+        driver.execute_script('''
+            window.scrollBy(0, 1000)
+        ''')
+        time.sleep(2)
+    videos = driver.find_elements_by_tag_name('ytd-video-renderer')
+    len( videos )
+    for video in videos[:20]:
+        target = video.find_element_by_id('video-title')
+        title  = target.get_attribute('title')
+        thumbnail = video.find_element_by_id('img').get_attribute('src')
+        video_url = target.get_attribute('href')
+        print( title, thumbnail, video_url )
+    youtube_list = list()
+    for video in videos[:20]:  
+        target    = video.find_element_by_id('video-title')
+        title     = target.get_attribute('title')
+        thumbnail = video.find_element_by_id('img').get_attribute('src')    
+        video_url = target.get_attribute('href')
+        youtube_list.append( {
+            't':title,
+            'n':thumbnail,
+            'v':video_url
+        } )
+    youtube_list
+
+    import pandas as pd
+    import pymysql
+    df = pd.DataFrame(youtube_list)
+
+    from sqlalchemy import create_engine
+    import pandas.io.sql as pSql
+    protocal = 'mysql+pymysql'
+    user     = 'admin'
+    password = 'pnudb960726!'
+    domain   = 'personaldb.cepsu2i8bkn5.ap-northeast-2.rds.amazonaws.com'
+    port = 3306
+    database = 'yaneodoo'
+    db_url = f'{protocal}://{user}:{password}@{domain}:{port}/{database}'
+    engine = create_engine(db_url, encoding = 'utf8')
+    conn = engine.connect()
+    df.to_sql(name = 'youtube', con = conn, if_exists ='replace',index = False)
+except Exception as e:
+    print(e)
+finally:
+    conn.close()
+    driver.close()
+    driver.quit()
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
