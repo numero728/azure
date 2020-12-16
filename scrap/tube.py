110a0d93043c3db0ea54a7034fb6ec8a4eb7010a
numero728 <numero728@naver.com>
2020-12-16 20:55:31 +0900

    dd

--- scrap/tube.py
+++ scrap/tube.py
@@ -6,7 +6,7 @@ import urllib
 import os
 
 try:
-    options=Options
+    options=Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--deisable-dev-shm-usage')
