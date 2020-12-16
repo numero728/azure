ce61b67d0e529f4c8b85b33d71659dcbe519e7c9
numero728 <numero728@naver.com>
2020-12-16 20:46:22 +0900

    dd

--- scrap/tube.py
+++ scrap/tube.py
@@ -56,7 +56,7 @@ try:
     db_url = f'{protocal}://{user}:{password}@{domain}:{port}/{database}'
     engine = create_engine(db_url, encoding = 'utf8')
     conn = engine.connect()
-    df.to_sql(name = 'youtube', con = conn, if_exists ='replace',index = False)
+    df.to_sql(name = 'youtube', con = conn, if_exists ='append',index = False)
 except Exception as e:
     print(e)
 finally:
